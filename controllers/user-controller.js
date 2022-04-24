fda89f3038fce18610b4184e94d3f596b829cc2a
Divya <divya.arigay@gmail.com>
2022-04-24 07:10:24 -0600

    creating user thought and reaction models

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,66 @@ const { User } = require('../models');
 
 const userController = {
   // the functions will go in here as methods
+   // get all pizzas
+   getAllUser(req, res) {
+    User.find({})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one pizza by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then(dbUserData => {
+        // If no pizza is found, send 404
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // Creating a new user
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update user by Id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+//Delete a User
+deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+
 };
 
 module.exports = userController;
